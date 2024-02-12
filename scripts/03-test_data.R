a5464cde8e2b2ed9763a42096ff4b89e19923f81
Yichen Ji <yic.ji@mail.utoronto.ca>
2024-02-12 00:13:13 -0500

    upload cleaning

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -1,6 +1,6 @@
 #### Preamble ####
 # Purpose: Test the analysis data to make sure they are quilified to process in further steps
-# Author: Yichen Ji, Shuhan Yang, Xiaoxu Liu
+# Author: Yichen Ji, Xiaoxu Liu
 # Date: 11 February 2023
 # Contact: yic.ji@mail.utoronto.ca, xiaoxu.liu@mail.utoronto.ca
 # License: MIT
@@ -12,8 +12,34 @@
 library(tidyverse)
 library(testthat)
 
-lgbtq <- read_csv("data/analysis_data/lgbtq.csv")
-billion <- read_csv("data/analysis_data/billion.csv")
+lgbtq_educ <- read_csv("data/analysis_data/lgbtq_education.csv")
+lgbtq_gender <- read_csv("data/analysis_data/lgbtq_gender.csv")
 partisans <- read_csv("data/analysis_data/partisans.csv")
 
 #### Test data ####
+
+
+test_that("Column names match expected names", {
+  expect_equal(names(partisans), c("treatment", "handle", "gender", "race", "educ", "partisan"))
+})
+
+
+
+expected_educ_levels <- c("High school graduate", "College", "Postgraduate")
+
+test_that("educ column in lgbtq_educ contains only the expected levels", {
+  content <- lgbtq_educ
+  unique_educ_values <- unique(content$educ)
+  expect_true(all(unique_educ_values %in% expected_educ_levels))
+})
+
+
+
+expected_gender_levels <- c("Male", "Female")
+
+test_that("educ column in lgbtq_gender contains only the expected levels", {
+  content <- lgbtq_gender
+  unique_gender_values <- unique(content$gender)
+  expect_true(all(unique_gender_values %in% expected_gender_levels))
+})
+
