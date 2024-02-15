59be29a44a261c18d8cfcff1f5749953bfe12dba
Yichen Ji <yic.ji@mail.utoronto.ca>
2024-02-15 03:09:59 -0500

    clean final update

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -49,15 +49,21 @@ iit_partisans_selected <- iit_partisans_selected %>%
   mutate(educ = str_replace(educ, "Postgraduate \\(e.g. Masters\\)", "Postgraduate"))
 
 
+#### Save data ####
+write.csv(iit_lgbtq_selected, "data/analysis_data/lgbtq.csv", row.names = FALSE)
+write.csv(iit_billion_selected, "data/analysis_data/billion.csv", row.names = FALSE)
+write.csv(iit_partisans_selected, "data/analysis_data/partisans.csv", row.names = FALSE)
+
+
 ### Calculate the percentage of different people's moderation to same treatment ###
 
 ## Target: LGBTQ ##
 #gender
-counts <- lgbtq %>%
+counts <-iit_lgbtq_selected %>%
   group_by(treatment,gender,handle) %>%
   summarise(count = n(), .groups = "drop") 
 
-total_counts <- lgbtq %>%
+total_counts <- iit_lgbtq_selected %>%
   group_by(treatment, gender) %>%
   summarise(total = n(), .groups = "drop") %>%
   ungroup()
@@ -72,12 +78,12 @@ result_l_gender <- percentages %>%
 
 
 #race
-counts_race <- lgbtq %>%
+counts_race <- iit_lgbtq_selected %>%
   filter(!is.na(race)) %>%
   group_by(treatment,race,handle) %>%
   summarise(count = n(), .groups = "drop")
 
-total_counts_race <- lgbtq %>%
+total_counts_race <- iit_lgbtq_selected %>%
   filter(!is.na(race)) %>%
   group_by(treatment, race) %>%
   summarise(total = n(), .groups = "drop") %>%
@@ -92,12 +98,12 @@ result_l_race <- percentages_race %>%
 
 
 #education
-counts_edu <- lgbtq %>%
+counts_edu <- iit_lgbtq_selected %>%
   filter(!is.na(educ)) %>%
   group_by(treatment,educ,handle) %>%
   summarise(count = n(), .groups = "drop")
 
-total_counts_edu <- lgbtq %>%
+total_counts_edu <- iit_lgbtq_selected %>%
   filter(!is.na(educ)) %>%
   group_by(treatment, educ) %>%
   summarise(total = n(), .groups = "drop") %>%
@@ -115,11 +121,11 @@ result_l_edu <- percentages_edu %>%
 ## Target: Billionaire ##
 
 #gender
-count <- billion %>%
+count <- iit_billion_selected %>%
   group_by(treatment,gender,handle) %>%
   summarise(count = n(), .groups = "drop")
 
-total_counts <- billion %>%
+total_counts <- iit_billion_selected %>%
   group_by(treatment, gender)%>%
   summarise(total = n(), .groups = "drop") %>%
   ungroup()
@@ -133,12 +139,12 @@ result_b_gender <- percentages %>%
   dplyr::select(treatment, gender, handle, percentage)
 
 #race
-counts_race <- billion %>%
+counts_race <- iit_billion_selected %>%
   filter(!is.na(race)) %>%
   group_by(treatment,race,handle) %>%
   summarise(count = n(), .groups = "drop")
 
-total_counts_race <- billion %>%
+total_counts_race <- iit_billion_selected %>%
   filter(!is.na(race)) %>%
   group_by(treatment, race) %>%
   summarise(total = n(), .groups = "drop") %>%
@@ -153,12 +159,12 @@ result_b_race <- percentages_race %>%
 
 
 #education
-counts_edu <- billion %>%
+counts_edu <- iit_billion_selected %>%
   filter(!is.na(educ)) %>%
   group_by(treatment,educ,handle) %>%
   summarise(count = n(), .groups = "drop")
 
-total_counts_edu <- billion %>%
+total_counts_edu <- iit_billion_selected %>%
   filter(!is.na(educ)) %>%
   group_by(treatment, educ) %>%
   summarise(total = n(), .groups = "drop") %>%
@@ -173,14 +179,24 @@ result_b_edu <- percentages_edu %>%
   dplyr::select(treatment, educ, handle, percentage) 
 
 
+#### Clean special character ###
+
+result_l_gender <- result_l_gender %>%
+  mutate(handle = gsub("Suspend the person’s account", "Suspend the person's account", handle))
+result_l_race <- result_l_race %>%
+  mutate(handle = gsub("Suspend the person’s account", "Suspend the person's account", handle))
+result_l_edu <- result_l_edu %>%
+  mutate(handle = gsub("Suspend the person’s account", "Suspend the person's account", handle))
+
+result_b_gender <- result_b_gender %>%
+  mutate(handle = gsub("Suspend the person’s account", "Suspend the person's account", handle))
+result_b_race <- result_b_race %>%
+  mutate(handle = gsub("Suspend the person’s account", "Suspend the person's account", handle))
+result_b_edu <- result_b_edu %>%
+  mutate(handle = gsub("Suspend the person’s account", "Suspend the person's account", handle))
 
 
 #### Save data ####
-write.csv(iit_lgbtq_selected, "data/analysis_data/lgbtq.csv", row.names = FALSE)
-write.csv(iit_billion_selected, "data/analysis_data/billion.csv", row.names = FALSE)
-write.csv(iit_partisans_selected, "data/analysis_data/partisans.csv", row.names = FALSE)
-
-
 write.csv(result_l_gender, "data/analysis_data/lgbtq_gender.csv", row.names = FALSE)
 write.csv(result_l_race, "data/analysis_data/lgbtq_race.csv", row.names = FALSE)
 write.csv(result_l_edu, "data/analysis_data/lgbtq_education.csv", row.names = FALSE)
